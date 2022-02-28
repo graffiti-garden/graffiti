12006094e7fda33e3805b99f8137dd1a778f7013
theia <theia@mit.edu>
2022-02-28 11:57:20 -0500

    [graffiti-x-vue]: Added query subscriber

--- graffiti-x-vue/graffiti-tools.js
+++ graffiti-x-vue/graffiti-tools.js
@@ -7,6 +7,9 @@ export default class GraffitiTools {
     this.origin = origin
     this.auth = new Auth(this.origin)
     this.querySocket = new QuerySocket(this.origin, this.auth)
+    this.QuerySubscriber = querySubscriber(
+      this.querySocket,
+      this.queryMany.bind(this))
   }
 
   get mySignature() {
@@ -23,7 +26,7 @@ export default class GraffitiTools {
     })
   }
 
-  async queryMany(query, time=0, limit=100, skip=0) {
+  async queryMany(query, limit, skip=0, time=0) {
     return await this.auth.request('post', 'query_many', {
       query: query,
       time: time,
@@ -32,19 +35,60 @@ export default class GraffitiTools {
     })
   }
 
-  async queryOne(query, time=0, skip=0) {
+  async queryOne(query, skip=0, time=0) {
     return await this.auth.request('post', 'query_one', {
       query: query,
       time: time,
       skip: skip
     })
   }
+}
 
-  async querySocketAdd(query_id, query, callback) {
-    return await this.querySocket.addQuery(query_id, query, callback)
-  }
+function querySubscriber(querySocket, queryMany) {
+  return class {
+    constructor(query, callback) {
+      this.query = query
+      this.callback = callback
+      this.queryTime = null
+      this.numPreceding = 0
 
-  async querySocketRemove(query_id) {
-    return await this.querySocket.removeQuery(query_id)
+      // Generate a random query ID
+      this.queryID = Math.random().toString(16).substr(2, 14)
+    }
+
+    async open() {
+      this.queryTime = await querySocket.addQuery(
+        this.queryID,
+        this.query,
+        this.callback)
+    }
+
+    async close() {
+      this.queryTime = null
+      this.numPreceding = 0
+      return await this.querySocket.removeQuery(this.queryID)
+    }
+
+    async rewind(limit) {
+      // Open if we haven't already
+      if (this.queryTime == null) {
+        await this.open()
+      }
+
+      // Fetch #(limit) preceding query matches
+      const preceding = await queryMany(
+        this.query,
+        limit,
+        this.numPreceding,
+        this.queryTime
+      )
+      this.numPreceding += preceding.length
+
+      // Call the callback on each of them
+      preceding.map(this.callback)
+
+      // Return whether or not we have completed
+      return preceding.length < limit
+    }
   }
 }
