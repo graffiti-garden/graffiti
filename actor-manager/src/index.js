77ad3a0edba5be3483fbf3e6d0acd7a64477d9c8
theia <theia@mit.edu>
2023-06-12 18:59:42 -0400

    [actor-manager]: Add a cookie so the page is visited

--- actor-manager/src/index.js
+++ actor-manager/src/index.js
@@ -5,6 +5,10 @@ import App from './components/App.vue'
 import Actors from './components/Actors.vue'
 import Apps from './components/Apps.vue'
 
+// Mark the page as visited
+document.cookie =
+`visited=true; expires=${(new Date(2e13)).toGMTString()}; SameSite=None`
+
 const Router = createRouter({
   history: ["localhost","127.0.0.1"].includes(window.location.hostname)?
     createWebHashHistory() : createWebHistory(),
