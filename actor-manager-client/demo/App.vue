0e92bc4288c86c072ea4ef7942d9b546ae3bacfd
theia <theia@mit.edu>
2023-07-25 17:05:24 -0400

    [actor-manager-client]: Added verification

--- actor-manager-client/demo/App.vue
+++ actor-manager-client/demo/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { ref } from 'vue'
+  import { ref, reactive } from 'vue'
   import ActorClient from '../client.js'
 
   const ac = new ActorClient()
@@ -7,6 +7,8 @@
   const actorID = ref('')
   const message = ref('')
   const signed = reactive({})
+  const verifyLoading = ref(false)
+  const verified = ref(false)
 </script>
 
 <template>
@@ -25,5 +27,19 @@
 
   <p>
     Your signed message: "{{ signed }}"
+  </p>
+
+  <form @submit.prevent="verifyLoading=true;ac.verify(signed).then(()=>verified=true).catch(()=> verified=false).finally(()=> verifyLoading=false)">
+    <input type="submit" value="Verify Signed Message">
+  </form>
+
+  <p>
+    Message Verified?
+    <template v-if="verifyLoading">
+      Loading...
+    </template>
+    <template v-else>
+      {{ verified }}
+    </template>
   </p>
 </template>
