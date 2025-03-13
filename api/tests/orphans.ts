4ab3661a67bf74b1137da0b087296921a22cd166
theia <theia@mit.edu>
2025-03-13 12:10:50 -0400

    [api]: test with continue

--- api/tests/orphans.ts
+++ api/tests/orphans.ts
@@ -1,6 +1,6 @@
 import { it, expect, describe, assert, beforeAll } from "vitest";
 import type { Graffiti, GraffitiSession } from "@graffiti-garden/api";
-import { randomPutObject, randomString } from "./utils";
+import { randomPutObject, randomString, nextStreamValue } from "./utils";
 
 export const graffitiOrphanTests = (
   useGraffiti: () => Pick<
@@ -37,6 +37,7 @@ export const graffitiOrphanTests = (
       let numResults = 0;
       for await (const orphan of orphanIterator2) {
         if (orphan.error) continue;
+        assert(!orphan.tombstone, "orphan is tombstone");
         if (orphan.value.url === putted.url) {
           numResults++;
           expect(orphan.value.lastModified).toBe(putted.lastModified);
@@ -53,6 +54,28 @@ export const graffitiOrphanTests = (
       // Wait for the put to be processed
       await new Promise((resolve) => setTimeout(resolve, 10));
 
+      expect(Object.keys(object.value).length).toBeGreaterThanOrEqual(1);
+      expect(Object.keys(object.value)[0]).toBeTypeOf("string");
+      const iterator1 = graffiti.recoverOrphans(
+        {
+          properties: {
+            value: {
+              properties: {
+                [Object.keys(object.value)[0]]: {
+                  type: "string",
+                },
+              },
+              required: [Object.keys(object.value)[0]],
+            },
+          },
+        },
+        session,
+      );
+      const value1 = await nextStreamValue(iterator1);
+      expect(value1.value).toEqual(object.value);
+      const returnValue = await iterator1.next();
+      assert(returnValue.done, "value2 is not done");
+
       const putNotOrphan = await graffiti.put<{}>(
         {
           ...putOrphan,
@@ -64,17 +87,27 @@ export const graffitiOrphanTests = (
       expect(putNotOrphan.url).toBe(putOrphan.url);
       expect(putNotOrphan.lastModified).toBeGreaterThan(putOrphan.lastModified);
 
+      // The tombstone will not appear to a fresh iterator
       const orphanIterator = graffiti.recoverOrphans({}, session);
       let numResults = 0;
       for await (const orphan of orphanIterator) {
         if (orphan.error) continue;
         if (orphan.value.url === putOrphan.url) {
           numResults++;
-          expect(orphan.value.lastModified).toBe(putNotOrphan.lastModified);
-          expect(orphan.value.channels).toEqual([]);
         }
       }
-      expect(numResults).toBe(1);
+      expect(numResults).toBe(0);
+
+      const iterator2 = returnValue.value.continue();
+      const value2 = await iterator2.next();
+      assert(
+        !value2.done && !value2.value.error,
+        "value2 is done or has error",
+      );
+      expect(value2.value.tombstone).toBe(true);
+      expect(value2.value.value.lastModified).toBe(putNotOrphan.lastModified);
+      expect(value2.value.value.channels).toEqual([]);
+      await expect(iterator2.next()).resolves.toHaveProperty("done", true);
     });
   });
 };
